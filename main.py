f70d89e549d97b8581566aeef4f9792bb9598abc
0-122-5 <0-122-5>
2020-11-25 18:26:57 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import sqlite3
+import sys
+import random
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.connection = sqlite3.connect("coffee.db")
+        self.cur = self.connection.cursor()
+        res = self.cur.execute("""SELECT * FROM coffees""").fetchall()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setHorizontalHeaderLabels(['id', 'название сорта', 'степень обжарки', 'молотый/в зернах',
+                                                    'описание вкуса', 'цена', 'объем упаковки' ])
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+        self.tableWidget.resizeColumnsToContents()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
